93d5bd82210babb45662f4202585713a30fdde74
Yash Jindal <yashjindal069@gmail.com>
2022-05-19 15:47:34 +0530

    Change Join color

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,23 +1,26 @@
-// @flow 
-import * as React from 'react';
+// @flow
+import * as React from "react";
 import "../card/Card.css";
 
-
 const Card = (props) => {
-    return (
-        <div>
-            <div class="card border" style={{padding:"10px", width : "50vh",minHeight:"250px"}}>
-                <div class="card-body d-flex flex-column" style={{marginTop : "30px"}}>
-                    <h5 class="card-title">{props.title}</h5>
-                    <p class="card-text">{props.desc}</p>
-                    <div className="cardButton">
-                        <button type="button" class="btn btn-success">JOIN</button>
-                    </div>
-                </div>
-            </div>
- 
+  return (
+    <div>
+      <div
+        class="card border"
+        style={{ padding: "10px", width: "50vh", minHeight: "250px" }}
+      >
+        <div class="card-body d-flex flex-column" style={{ marginTop: "30px" }}>
+          <h5 class="card-title">{props.title}</h5>
+          <p class="card-text">{props.desc}</p>
+          <div className="cardButton">
+            <button type="button" class="btn btn-primary">
+              JOIN
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
